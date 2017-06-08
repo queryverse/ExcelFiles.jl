196f1e9607e692194f5b7c75bc30ab93d3f708c8
David Anthoff <anthoff@berkeley.edu>
2017-06-08 14:09:00 -0700

    Add initial implementation

--- src/ExcelFiles.jl
+++ src/ExcelFiles.jl
@@ -1,5 +1,28 @@
 module ExcelFiles
 
-# package code goes here
+
+using ExcelReaders, IterableTables, DataValues, DataFrames
+import FileIO
+
+struct ExcelFile
+    filename::String
+    range::String
+    keywords
+end
+
+function load(f::FileIO.File{FileIO.format"Excel"}, range; keywords...)
+    return ExcelFile(f.filename, range, keywords)
+end
+
+IterableTables.isiterable(x::ExcelFile) = true
+IterableTables.isiterabletable(x::ExcelFile) = true
+
+function IterableTables.getiterator(file::ExcelFile)
+    df = contains(file.range, "!") ? readxl(DataFrame, file.filename, file.range; file.keywords...) : readxlsheet(DataFrame, file.filename, file.range)
+
+    it = getiterator(df)
+
+    return it
+end
 
 end # module
