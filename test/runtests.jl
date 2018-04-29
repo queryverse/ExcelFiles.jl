cdad5feed2a68a8fedaa404c67d26e7b3e1926c4
David Anthoff <anthoff@berkeley.edu>
2018-04-29 14:19:56 -0700

    Use Dates.Time

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,8 +25,8 @@ for (df, names) in full_dfs
     @test df[6] == [3., DataValues.NA, 3.5, 4]
     @test df[7] == ["FF", DataValues.NA, "GGG", "HHHH"]
     @test df[8] == [DataValues.NA, true, DataValues.NA, false]
-    @test df[9] == [Date(2015,3,3), DateTime(2015,2,4,10,14), Date(1988,4,9), ExcelReaders.Time(15,2,0)]
-    @test df[10] == [Date(1965,4,3), DateTime(1950,8,9,18,40), ExcelReaders.Time(19,0,0), DataValues.NA]
+    @test df[9] == [Date(2015,3,3), DateTime(2015,2,4,10,14), Date(1988,4,9), Dates.Time(15,2,0)]
+    @test df[10] == [Date(1965,4,3), DateTime(1950,8,9,18,40), Dates.Time(19,0,0), DataValues.NA]
     @test eltype(df[11]) == ExcelReaders.ExcelErrorCell
     @test df[12][1] isa ExcelReaders.ExcelErrorCell
     @test df[12][2] isa ExcelReaders.ExcelErrorCell
